4f04399a3dc621b6263bf716002c88eae8c72184
okechukwu95dev <62656119+okechukwu95dev@users.noreply.github.com>
2025-05-08 09:54:53 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-  <header>
-          <div class="logo-container">
-            <h3><img src="logo.png" alt="Site Icon" class="logo">JPMorgan Chase</h3>
-          </div>
-        </header>
+<header>
+  <div class="logo-container">
+    <img src="logo.png" alt="Site Icon" style="vertical-align: middle; width: 24px; height: 24px; margin-right: 10px;">
+    <h3>JPMorgan Chase</h3>
+  </div>
+</header>
